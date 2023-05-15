952495c71c05a6b5a0e0b132ab457b043754cd91
JohannSiemens <johann.siemens85@gmx.de>
2023-05-15 10:53:22 +0200

    refactored the index.js of the api/places route

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -5,22 +5,30 @@ export default async function handler(request, response) {
   await dbConnect();
 
   if (request.method === "GET") {
-    const place = await Place.find();
+    try {
+      const place = await Place.find();
 
-    return response.status(200).json(place);
+      return response.status(200).json(place);
+    } catch {
+      response.status(500).json({ status: "Bad Response" });
+    }
   } else if (request.method === "POST") {
-    const place = request.body;
+    try {
+      const place = request.body;
 
-    const newPlace = {
-      name: place.name,
-      location: place.location,
-      image: place.image,
-      mapURL: place.mapURL,
-      description: place.description,
-    };
+      const newPlace = {
+        name: place.name,
+        location: place.location,
+        image: place.image,
+        mapURL: place.mapURL,
+        description: place.description,
+      };
 
-    await Place.create(newPlace);
+      await Place.create(newPlace);
 
-    return response.status(200).json({ status: "New place created!" });
+      return response.status(200).json({ status: "New place created!" });
+    } catch {
+      response.status(500).json({ status: "Bad Response" });
+    }
   }
 }
